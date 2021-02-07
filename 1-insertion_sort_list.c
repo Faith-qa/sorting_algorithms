9b8f28bebbd66b5c84c5115419b8e00617f866f2
Faith-qa <atienofaith12@gmail.com>
2021-02-07 21:42:01 +0300

    implementation of insertion algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,74 +1,49 @@
-
 #include "sort.h"
+#include <stdio.h>
 /**
- *SortedInsert - inserts a new nnode in a sorted way
- *@newnode : node to be inserted
- *@list : pointer to the head of the double linked list
- *Return : void
- */
-
-void InsertSortedNode(listint_t **list, listint_t *newnode)
-{
-  listint_t* current;
-  /*if list is empty...*/
-  if(*list == NULL)
-    newnode = *list;
-
-  /*if the new node is to be inserted at the begining of list..*/
-  else if ((*list)->n >= newnode->n)
-    {
-      newnode->next = *list;
-      newnode->next->prev = newnode;
-      *list = newnode;
-    }
-  else
-    {
-      current = *list;
-
-      /*locate the node after which the new node is to be inserted*/
-      while (current->next != NULL && current->next->n < newnode->n)
-	current = current->next;
-      newnode->next = current->next;
-
-      /*if the newnode is not inserted at the end of the list..*/
-      if (current->next != NULL)
-	newnode->next->prev = newnode;
-
-      current->next = newnode;
-      newnode->prev = current;
-    }
-}
-/**
- *insertion_sort_list - sorts a double linked list with insertion method
- *@list: list to be sorted
- *Return: print sorted list
- */
-
+ * insertion_sort_list - insertion sort algorithm for doubly linked list
+ * @list: pointer to array to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
-  /*initialize 'sorted' - a sorted double linked list*/
-  listint_t *sorted;
-  listint_t *current;
-  listint_t *tmp; /*temporarily holds a node in waiting*/
 
-  /*review the list and insert every node to 'sorted'*/
-  current = *list;
-  while (current)
-    {
-      tmp = current->next;
+listint_t *node, *tmp, *next, *tmp_next, *tmp_prev;
+int i = 0;
 
-      /*making current the newnode for insertion*/
-      current->prev = current->next  = NULL;
+if (list == NULL || (*list)->next == NULL)
+	return;
+node = *list;
+node = node->next;
+while (node != NULL)
+{
+	tmp = node;
+	next = node->next;
+	tmp_next = tmp->next; /*Will be NULL for the last Node*/
+	tmp_prev = tmp->prev;
 
-      /*insert current in sorted using the InsertSortedNode function*/
-      InsertSortedNode(&sorted, current);
-
-      /*update current*/
-      current = tmp;
-    }
-
-   /*update list to point to sorted list*/
-      *list = sorted;
-      print_list(sorted);
-
+	while (tmp->prev != NULL && tmp_prev->n > tmp->n)
+	{
+		if (tmp_next != NULL)
+			tmp_next->prev = tmp_prev;
+		tmp_prev->next = tmp_next;
+		if (tmp_prev->prev == NULL)
+		{
+			tmp_prev->prev = tmp;
+			*list = tmp;
+			tmp->prev = NULL;
+		}
+		else
+		{
+			tmp->prev = tmp_prev->prev;
+			tmp_prev->prev->next = tmp;
+			tmp_prev->prev = tmp;
+		}
+		tmp->next = tmp_prev;
+		tmp_prev = tmp->prev;
+		tmp_next = tmp->next;
+		print_list(*list);
+	}
+	node = next;
+	i++;
+}
 }
